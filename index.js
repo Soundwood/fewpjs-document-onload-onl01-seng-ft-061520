d6c8696976ba070b39be0c276e6c3c93dc10f605
Soundwood <j.benhogan@gmail.com>
2020-09-19 18:52:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    modifyText()
+});
+
+function modifyText() {
+    document.getElementById("text").innerHTML = "This is really cool!"
+}
